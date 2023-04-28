5866eaf4c18b62bf9f9fd7473bcf7e0c07bb7bc7
Paul Bauman <psbauman@ucsc.edu>
2023-04-27 21:28:34 -0700

    int_product__checkouts adjustments

--- greenery/models/marts/product/intermediate/int_product__checkouts.sql
+++ greenery/models/marts/product/intermediate/int_product__checkouts.sql
@@ -8,12 +8,17 @@
 select distinct
 
   e.session_id,
-  e.product_id,
+  eo.product_id,
   p.name as product_name
 
 from {{ ref('stg_postgres__events') }} as e
 
 left join {{ ref('stg_postgres__products')}} as p
-on p.product_id = e.product_id
+on p.product_id = eo.product_id
+
+left join {{ ref('stg_postgres__events') }} as eo
+ on eo.user_id = e.user_id
+ and eo.session_id = e.session_id
+ and eo.order_id is not null
 
 where e.event_type = 'checkout'
