35ff62396ab499fc6272ebf3fdfed6b1b0186400
Paul Bauman <psbauman@ucsc.edu>
2023-04-28 04:22:27 +0000

    fixed int_product__checkouts

--- greenery/models/marts/product/intermediate/int_product__checkouts.sql
+++ greenery/models/marts/product/intermediate/int_product__checkouts.sql
@@ -12,7 +12,8 @@ select distinct
   p.name as product_name
 
 from {{ ref('stg_postgres__events') }} as e
-where e.event_type = 'checkout'
 
 left join {{ ref('stg_postgres__products')}} as p
 on p.product_id = e.product_id
+
+where e.event_type = 'checkout'
