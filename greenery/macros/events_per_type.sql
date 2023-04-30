097be774735fcdd5664b98c00650a54a3fa86389
Paul Bauman <psbauman@ucsc.edu>
2023-04-30 15:13:35 -0700

    welp... bummer. Macros don't work in model definition ymls

--- greenery/macros/events_per_type.sql
+++ greenery/macros/events_per_type.sql
@@ -1,7 +1,6 @@
 {% macro events_per_type(table_name, type_column) %} 
 
-    {% set event_types = dbt_utils.get_column_values(source( 'postgres', table_name ), type_column)
-    %}
+    {% set event_types = dbt_utils.get_column_values(table_name, type_column) %}
 
     {% for event_type in event_types %}
         , sum(case when event_type = '{{ event_type }}' then 1 else 0 end) AS total_{{ event_type }}
--- greenery/models/marts/product/intermediate/int_product_session_types.sql
+++ greenery/models/marts/product/intermediate/int_product_session_types.sql
@@ -1,6 +1,6 @@
 select
     user_id,
     session_id
-    {{ events_per_type('events','event_type') }}
-from source( 'postgres', 'events' )
+    {{ events_per_type(ref('stg_postgres__events'),'event_type') }}
+from ref('stg_postgres__events')
 group by 1,2
