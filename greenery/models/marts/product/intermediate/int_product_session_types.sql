73c0b01ee8dfa08751cf0fd87b4536dbac1bf742
Paul Bauman <psbauman@ucsc.edu>
2023-04-30 15:19:25 -0700

    welp... bummer. Macros don't work in model definition ymls

--- greenery/models/marts/product/intermediate/int_product_session_types.sql
+++ greenery/models/marts/product/intermediate/int_product_session_types.sql
@@ -2,5 +2,5 @@ select
     user_id,
     session_id
     {{ events_per_type(ref('stg_postgres__events'),'event_type') }}
-from ref('stg_postgres__events')
+from {{ ref('stg_postgres__events') }}
 group by 1,2
